f55c85b102a0641e2fc1a0bd73747e3aeda17622
mauro Leos <mauroleos94@gmail.com>
2020-11-20 09:39:23 -0600

    Installed React and added Header

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDLpTCyTWXPA5GjD3rBHxFp8wSbboDs_bs",
+    authDomain: "clone-e30b3.firebaseapp.com",
+    databaseURL: "https://clone-e30b3.firebaseio.com",
+    projectId: "clone-e30b3",
+    storageBucket: "clone-e30b3.appspot.com",
+    messagingSenderId: "529326958348",
+    appId: "1:529326958348:web:374a27e7aa7f08897f0b29",
+    measurementId: "G-6QX853F4MJ"
+  };
