1f3320b2a640c403d515d1b0403e53e5c91aeebc
mauro Leos <mauroleos94@gmail.com>
2020-11-26 09:22:42 -0700

    Added features ad styling

--- src/Product.js
+++ src/Product.js
@@ -3,14 +3,21 @@ import "./Product.css"
 
 function Product () {
     return (
-        <div className="Product">
+        <div className="product">
             <div className="product__info">
                 <p>The lean startup</p>
                 <p className="product__price">
                     <small>$</small>
                     <strong>19.99</strong>
                 </p>
+                <div className="product__rating">
+                    <p>🌟</p> 
+                </div>
             </div>
+
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" al="" />
+
+            <button>Add to Basket</button>
         </div>
     )
 }
