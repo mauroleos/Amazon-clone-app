8ecd0f2f863268d4687e162ce74e911b1f985b57
mauro Leos <mauroleos94@gmail.com>
2020-12-21 20:33:15 -0600

    Added useEffect to App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,36 @@ import Home from "./Home"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout"
 import Login from "./Login";
+import { auth } from "./firebase"
+import { useStateValue } from "./StateProvider"
 
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+  //will only run once 
+
+  auth.onAuthStateChanged(authUser => {
+    console.log("The user is", authUser)
+
+    if (authUser) {
+      //user just logged in / user was logged in
+
+      dispatch({
+        type: "SET_USER",
+        user: authUser
+      })
+    } else {
+      //the user is logged out
+    dispatch({
+      type: "SET_USER",
+      user: null
+    })
+    }
+  })
+}, [])
+
   return (
     //BEM
     <Router>
